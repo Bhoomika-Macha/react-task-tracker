2a8413de4922be2cc2cef316d23ef08c56fd4467
Bhoomika Macha <bhoomikamacha03@gmail.com>
2026-01-07 18:05:09 +0530

    Task Tracker

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import PropTypes from 'prop-types'
-import '../App.css'
+import '../index.css'
 import Button from './Button'
 
-const Header = ({title='Task Tracker'}) => {
-  const onClick = ()=>{
-    console.log('click')
-  } 
+const Header = ({title='Task Tracker', onAdd, showAdd}) => {
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color='green' text='Add' onClick={onClick}/>
+      <Button 
+      color={showAdd ? 'red': 'green'} 
+      text={showAdd ? 'Close' : 'Add'}
+      onClick={onAdd}/>
     </header>
   )
 }
